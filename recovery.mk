0cff0f2a9575078394fd213904a0a531aa5e7de0
Utkarsh Tiwari <javitri61@gmail.com>
2016-08-27 11:34:10 +0530

    Fix lzma compression erroe

--- recovery.mk
+++ recovery.mk
@@ -1,17 +1,18 @@
 LZMA_BIN := /usr/bin/lzma
 
-
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
-		$(recovery_ramdisk) \
+		$(recovery_uncompressed_ramdisk) \
+		$(TF_BLOBIFIER) \
 		$(recovery_kernel)
-	@echo ----- Compressing recovery ramdisk with lzma ------
+ @ECHO ----- Compressing recovery ramdisk with lzma ------
 	rm -f $(recovery_uncompressed_ramdisk).lzma
 	$(LZMA_BIN) $(recovery_uncompressed_ramdisk)
-	$(hide) cp $(recovery_uncompressed_ramdisk).lzma $(recovery_ramdisk)
-	@echo ----- Making recovery image ------
-	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) --output $@
-	@echo ----- Made recovery image -------- $@
-	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+	$(hide) cp $(recovery_uncompressed_ramdisk).lzma $(recovery_uncompressed_ramdisk)
+ @ECHO ----- Making recovery image ------
+	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) --output $@.orig
+	$(TF_BLOBIFIER) $@ SOS $@.orig
+ @ECHO ----- Made recovery image -------- $@
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)raw)
 
 
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
