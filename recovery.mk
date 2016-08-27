3eb413146dd6d7b7352055fd4a5aa24437e010e8
Utkarsh Tiwari <javitri61@gmail.com>
2016-08-27 23:25:55 +0530

    Update recovery.mk

--- recovery.mk
+++ recovery.mk
@@ -2,16 +2,16 @@ LOCAL_PATH := $(call my-dir)
 
 LZMA_RAMDISK_BOOT := $(PRODUCT_OUT)/ramdisk.lzma
 LZMA_RAMDISK_RECOVERY := $(PRODUCT_OUT)/ramdisk-recovery.lzma
+LZMA_COMPRESS_COMMAND := xz --format=lzma --lzma1=dict=16MiB -
 
-$(LZMA_RAMDISK_BOOT): $(INSTALLED_RAMDISK_TARGET)
+$(LZMA_RAMDISK_BOOT): $(MKBOOTFS) $(INTERNAL_RAMDISK_FILES)
 	@echo -e ${CL_GRN}"----- Making LZMA boot ramdisk ------"${CL_RST}
-	$(hide) gzip -d < $(INSTALLED_RAMDISK_TARGET) > $(PRODUCT_OUT)/ramdisk.cpio
-	$(hide) xz --format=lzma --lzma1=dict=16MiB -9 < $(PRODUCT_OUT)/ramdisk.cpio > $@
+	$(hide) $(MKBOOTFS) -d $(TARGET_OUT) $(TARGET_ROOT_OUT) | $(LZMA_COMPRESS_COMMAND) > $@
 	@echo -e ${CL_CYN}"Made LZMA boot ramdisk: $@"${CL_RST}
 
 $(LZMA_RAMDISK_RECOVERY): $(recovery_uncompressed_ramdisk)
 	@echo -e ${CL_GRN}"----- Making LZMA recovery ramdisk ------"${CL_RST}
-	$(hide) xz --format=lzma --lzma1=dict=16MiB -9 < $(PRODUCT_OUT)/ramdisk-recovery.cpio > $@
+	$(hide) $(LZMA_COMPRESS_COMMAND) < $(PRODUCT_OUT)/ramdisk-recovery.cpio > $@
 	@echo -e ${CL_CYN}"Made LZMA recovery ramdisk: $@"${CL_RST}
 
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(LZMA_RAMDISK_BOOT)
