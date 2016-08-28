aa87f72f5656464a39505aaeeb906404284f06f9
Utkarsh Tiwari <javitri61@gmail.com>
2016-08-28 17:46:36 +0530

    Update

--- recovery.mk
+++ recovery.mk
@@ -1,14 +1,13 @@
-
 LZMA_BIN := /usr/bin/lzma
 
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
-		$(recover_ramdisk) \
+		$(recovery_ramdisk) \
 		$(recovery_kernel)
 	@echo ----- Compressing recovery ramdisk with lzma ------
 	rm -f $(recovery_uncompressed_ramdisk).lzma
 	$(LZMA_BIN) $(recovery_uncompressed_ramdisk)
-	$(hide) cp $(recovery_uncompressed_ramdisk).lzma $(recover_ramdisk)
+	$(hide) cp $(recovery_uncompressed_ramdisk).lzma $(recovery_ramdisk)
 	@echo ----- Making recovery image ------
 	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) --output $@
 	@echo ----- Made recovery image -------- $@
