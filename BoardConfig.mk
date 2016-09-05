4631becf62fbcd3881b0f344a41c8d69b6abff2d
DevUt <javitri61@gmail.com>
2016-09-06 01:07:21 +0530

    zRam

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,9 +78,12 @@ COMMON_GLOBAL_CFLAGS             += -DUSES_TI_MAC80211
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/htc/endeavoru
-TARGET_KERNEL_CONFIG := twrp_endeavoru_defconfig 
+TARGET_KERNEL_CONFIG := underscore_defconfig
 
-# Art Tuning 
+# Charge mode
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/htc_lpm/lpm_mode
+
+
 ART_BUILD_TARGET_DEBUG := false
 ART_USE_OPTIMIZING_COMPILER := true
 
