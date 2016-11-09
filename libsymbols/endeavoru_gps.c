075d386de9b884d496ef86fb8299d31c90606d9e
DevUt <javitri61@gmail.com>
2016-11-09 16:13:05 +0530

    Revert "Revert "Revert "Revert " Revert c5a4d9559252497b8012be97241c610c70742317""""
    This reverts commit 75055b9f51b26f20d6d151a50834239db362dbf1.

--- libsymbols/endeavoru_gps.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <pthread.h>
-
-#include <utils/Log.h>
-
-pthread_mutex_t _ZN7android9SingletonINS_13SensorManagerEE5sLockE = PTHREAD_MUTEX_INITIALIZER;
