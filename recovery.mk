5481f8e7627b175b4334bd576515fadedb5c2dda
DevUt <javitri61@gmail.com>
2017-03-24 23:09:36 +0530

    Revert "Remove bootimage building for now"
    This reverts commit 861ff882ecbc4cfeaef6ed1e060a08bbc5b9a207.

--- recovery.mk
+++ recovery.mk
@@ -14,3 +14,7 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 
 
+$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
+	$(call pretty,"Target boot image: $@")
+	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) --output $@
+$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
